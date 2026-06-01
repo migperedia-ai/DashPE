3191624e702a7e30b64139be63c80b19b6726b8f
migperedia-ai <migperedia@gmail.com>
2026-06-01 12:08:34 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { getTasks } from "@/lib/sheets";
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  let tasks = [];
+  let tasks: any[] = [];
   let error = "";
 
   try {
